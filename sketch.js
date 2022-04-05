53d7f6751b0804cbdbc64efa9ce026313bd5826a
DEEISH03 <103080292+DEEISH03@users.noreply.github.com>
2022-04-05 18:58:20 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var box
+
+function setup() {
+  createCanvas(400, 400);
+  box = createSprite(250,60,30,30)
+box.shapeColor="red"
+}
+
+function draw() 
+{
+  background(30);
+
+  drawSprites()
+
+}
+
+
+
+
